c745405fd218ae053caf65d04d3b161b548eda98
AwesomeJT99 <joshataylor99@gmail.com>
2018-11-04 16:04:45 -0800

    Fixed issues

--- username.sh
+++ username.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
-# zip.sh
+# username.sh
 echo "The only characters that can be used are lowercase letters, digits, and underscores"
 echo "It also must start with a lowercase letter, and it must be between 3 and 12 characters"
 echo "Please enter a username"
 read USRNM
 
-echo $USRNM
-
-while echo $USRNM | egrep -v "^[qwertyuiopasdfghjklzxcvbnm][abcdefghijklmnopqrstuvwxyz_,0-9]{3,12}$" > /dev/null 2>&1
+while echo $USRNM | egrep -v "^[qwertyuiopasdfghjklzxcvbnm][abcdefghijklmnopqrstuvwxyz_0-9]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username!"
 	echo "Enter a new username "
